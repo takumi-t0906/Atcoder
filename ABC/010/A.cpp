1b7af4d155191f1e874b3e87695c8a827d335a14
takumi-t0906 <cylinder0906@gmail.com>
2020-06-30 23:26:35 +0900

    add ABC10-A,15-A,29-A,38-A,41-A
    ABC10-A,15-A,29-A,38-A,41-A complete

---
+++ ABC/010/A.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    cout << s << "pp" << endl;
+
+    return 0;
+}
---
+++ ABC/029/A.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string w;
+    cin >> w;
+
+    cout << w + "s" << endl;
+
+    return 0;
+}
